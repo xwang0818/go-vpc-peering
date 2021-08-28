481479a30273883f89a8fb54f95597d3377138a7
Xiang Wang <xiang.wang@cloudimperiumgames.com>
2021-08-28 00:37:04 -0500

    fix

--- main.go
+++ main.go
@@ -7,25 +7,10 @@ import (
 
 func main() {
     pulumi.Run(func(ctx *pulumi.Context) error {
-        // VPC
-        xwVpc, err := ec2.NewVpc(ctx, "xwVpc", &ec2.VpcArgs{
-            CidrBlock: pulumi.String("10.223.0.0/16"),
-            Tags: pulumi.StringMap{
-                "Name": pulumi.String("xw-vpc-pulumi-operater-test"),
-            },
-        })
-        if err != nil {
-            return err
-        }
-        // Subnet
-        _, err = ec2.NewSubnet(ctx, "xwSubnet", &ec2.SubnetArgs{
-            VpcId:            xwVpc.ID(),
-            CidrBlock:        pulumi.String("10.222.1.0/24"),
-            AvailabilityZone: pulumi.String("us-east-2a"),
-            Tags: pulumi.StringMap{
-                "Name": pulumi.String("xw-subnet-pulumi-operater-test"),
-            },
-        })
+    		// VPC
+    		_, err := ec2.NewVpc(ctx, "xwvpc", &ec2.VpcArgs{
+    			CidrBlock: pulumi.String("10.125.0.0/16"),
+    		})
         if err != nil {
             return err
         }
